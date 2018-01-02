afb102e2744972e74a7923538fa92ccbfd66fe88
admin <admin@10.137.138.150>
2018-01-02 17:05:50 +0900

    Chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,8 +1,11 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	//나의 첫번째 Java 프로그램
+	
 	public static void main(String[] args) {
+//		메인 메서드 안에 주석 달기
+		
 		System.out.println("Hello World~~");
 		System.out.println("환영합니다. 여러분!");
 		System.out.println("sysout이라 입력하고 큰트롤 스패이스를 누르세요.");
